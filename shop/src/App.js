0f8efc4e7dbe45ebf131054461bbb96eed4d182e
Diana Murtazina <murka8831@yandex.ru>
2022-09-27 13:31:33 +0400

    creating header, footer components, adding styles

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,5 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
-    return <div>Shop</div>;
+    return (
+        <div className="wrapper">
+            <Header />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
